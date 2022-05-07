c4e74c77ab99bcdac17984c7c22b50d2c106ac19
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-08 02:38:14 +0900

    feat: mariaDB Connector/C

---
+++ install/mariadb-connector-c.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Ref: https://mariadb.com/docs/clients/mariadb-connectors/connector-c/install/#connector-c-install-repo-install
+sudo apt-get update
+sudo apt-get install libmariadb3 libmariadb-dev
