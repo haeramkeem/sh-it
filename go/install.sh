c43f964a52f1792c6a3ee2565c930a03dc74a79b
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-07 23:51:49 +0900

    fix: minor changes

--- go/install.sh
+++ go/install.sh
@@ -3,7 +3,10 @@
 curl -L https://go.dev/dl/go1.18.1.linux-amd64.tar.gz -o go.tar.gz
 tar -xzvf go.tar.gz -C /usr/local
 
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-echo "export GOPATH=\$HOME/go" >> ~/.profile
 echo "export GOROOT=/usr/local/go" >> ~/.profile
-source ~/.profile
+echo "export GOPATH=\$HOME/go" >> ~/.profile
+echo "export PATH=\$PATH:\$GOROOT/bin" >> ~/.profile
+echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.profile
+
+echo "Installation done"
+echo "* Post installation: Type 'source ~/.profile' to load the env-variables"
