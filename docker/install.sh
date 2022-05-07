a2d4b0416a4df4489c2bb3076f617151cc577ece
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-08 02:10:38 +0900

    fix: docker compose installation

--- docker/install.sh
+++ docker/install.sh
@@ -17,6 +17,6 @@ https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 systemctl enable --now docker
 
