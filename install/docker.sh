bf61647cf9b7a14bb7e9ae649aad33ba39a3fd0d
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-10 11:11:34 +0900

    fix: use docker with non-root

--- install/docker.sh
+++ install/docker.sh
@@ -2,19 +2,28 @@
 
 # Ref: https://docs.docker.com/engine/install/ubuntu/
 
+# Uninstall old version
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
 # Install prerequisites
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
 # Install docker apt repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) \
 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-systemctl enable --now docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl enable --now docker
 
+# Post-installation
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
