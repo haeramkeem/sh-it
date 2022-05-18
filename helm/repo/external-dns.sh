b841dd7a164e1d243676bcb44a9243f31a8425fd
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-18 12:55:26 +0900

    feat: helm/repo/external-dns

---
+++ helm/repo/external-dns.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Ref: https://github.com/kubernetes-sigs/external-dns/tree/master/charts/external-dns
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
+helm repo update
