eded677cd3c15ddac9a29e41bdc8c6593f620da1
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-16 17:44:32 +0900

    fix: install docker w/ version

--- install/docker.sh
+++ install/docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Installation script for Docker
+# Maintained by @SaltWalks/coworking/2022
 # Ref: https://docs.docker.com/engine/install/ubuntu/
+
+# ARGs
+VER=${1:+"=$1"}                 # $1: (OPTIONAL) Docker version
 
 # Uninstall old version
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -9,7 +14,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
-# Install docker apt repo
+# Install docker APT repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
 sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) \
@@ -17,17 +22,16 @@ signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install Docker
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce$VER docker-ce-cli$VER containerd.io docker-compose-plugin
 sudo systemctl enable --now docker
 
 # Post-installation
 if [[ $USER != "root" ]]; then
-    UNAME=${1:-$USER}
-
     sudo groupadd docker
-    sudo usermod -aG docker $UNAME
+    sudo usermod -aG docker $USER
     newgrp docker
-    sudo chown "$UNAME":"$UNAME" /home/"$UNAME"/.docker -R
-    sudo chmod g+rwx "/home/$UNAME/.docker" -R
+    sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+    sudo chmod g+rwx "/home/$USER/.docker" -R
 fi
