54197959cb58f60d4182444f37f7fcb3154f907f
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-29 20:17:11 +0900

    fix: add recommended post-installation script for aws-cli

--- install/aws-cli.sh
+++ install/aws-cli.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Ref: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf aws/ awscliv2.zip
+
+echo ""
+echo "*** Do 'aws configure' to configure secrets like AWS access key ID and secret access key"
+echo "*** see: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html"
+echo ""
