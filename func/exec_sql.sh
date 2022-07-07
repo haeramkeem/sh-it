904d4310f0f600f6f808fb7461f6dc74f8fa07b5
@haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-07 11:12:42 +0900

    Update exec_sql.sh

--- func/exec_sql.sh
+++ func/exec_sql.sh
@@ -3,22 +3,22 @@
 function exec_sql {
     SQL=$(cat -)
 
-    if [ -z $DB_USERNAME ]; then
+    if [ -z "$DB_USERNAME" ]; then
         echo "Abort: DB_USERNAME not provided as env" 1>&2
         exit 1
     fi
 
-    if [ -z $DB_PASSWORD ]; then
+    if [ -z "$DB_PASSWORD" ]; then
         echo "Abort: DB_PASSWORD not provided as env" 1>&2
         exit 1
     fi
 
-    if [ -z $DB_DATABASE ]; then
+    if [ -z "$DB_DATABASE" ]; then
         echo "Abort: DB_DATABASE not provided as env" 1>&2
         exit 1
     fi
 
-    if [ -z $SQL ]; then
+    if [ -z "$SQL" ]; then
         echo "Abort: SQL not provided to STDIN" 1>&2
         exit 1
     fi
