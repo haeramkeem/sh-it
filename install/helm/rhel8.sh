acda76b7d3b80e7a9e1cddd5acc5c6ea780141a7
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-14 17:09:03 +0900

    feat: RHEL8 Helm installation script

---
+++ install/helm/rhel8.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Defaults
+VER="v3.9.1"
+OS="linux"
+ARCH="amd64"
+INSTALL_DIR="/usr/local/bin"
+
+# ARGs
+while getopts 'v:o:a:' opt; do
+    case "$opt" in
+        v) VER=${OPTARG:+"-$OPTARG"} ;;     # (OPTIONAL) - Helm version
+        o) OS=${OPTARG:+"-$OPTARG"} ;;      # (OPTIONAL) - OS name
+        a) ARCH=${OPTARG:+"-$OPTARG"} ;;    # (OPTIONAL) - Arch
+    esac
+done
+
+# Install
+curl -LO https://get.helm.sh/helm-$VER-$OS-$ARCH.tar.gz
+sudo mv $OS-$ARCH/helm $INSTALL_DIR/helm
+
+# Clean up
+rm -rf helm-$VER-$OS-$ARCH.tar.gz
+rm -rf $OS-$ARCH/
