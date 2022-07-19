89707491c63ab072d57fbef478cac0eb2a49b1cd
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-19 09:28:15 +0900

    fix: use symlink to use helm in root

--- install/helm/rhel8.sh
+++ install/helm/rhel8.sh
@@ -9,7 +9,7 @@ INSTALL_DIR="/usr/local/bin"
 # ARGs
 while getopts 'v:o:a:' opt; do
     case "$opt" in
-        v) VER=${OPTARG:+"-$OPTARG"} ;;     # (OPTIONAL) - Helm version
+        v) VER=${OPTARG} ;;                 # (OPTIONAL) - Helm version
         o) OS=${OPTARG} ;;                  # (OPTIONAL) - OS name
         a) ARCH=${OPTARG} ;;                # (OPTIONAL) - Arch
         i) INSTALL_DIR=${OPTARG} ;;         # (OPTIONAL) - Install directory
@@ -20,6 +20,7 @@ done
 curl -L https://get.helm.sh/helm-$VER-$OS-$ARCH.tar.gz -o helm.tar.gz
 tar -xzvf helm.tar.gz
 sudo mv $OS-$ARCH/helm $INSTALL_DIR/helm
+sudo ln -s $INSTALL_DIR/helm /usr/bin/helm
 
 # Clean up
 rm -rf helm.tar.gz
