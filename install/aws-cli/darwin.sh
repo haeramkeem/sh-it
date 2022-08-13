baa56baedb0dd284666e33179e80b5ebd4e3f164
haeramkeem <ewqdsacxz2345@gmail.com>
2022-08-13 13:57:52 +0900

    feat: aws-cli darwin installer

---
+++ install/aws-cli/darwin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Source: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
