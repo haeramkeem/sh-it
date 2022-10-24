38bf90de5ca53af028dd97475d594dc8f07f489c
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-24 11:15:59 +0900

    feat: terminal basic seetup

---
+++ rcs/terminal-colorscheme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download terminal color scheme
+COLOR_SCHEME=Espresso
+
+curl -Lo ~/Downloads/$COLOR_SCHEME.terminal \
+    https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/terminal/$COLOR_SCHEME.terminal
---
+++ rcs/terminal-font.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
