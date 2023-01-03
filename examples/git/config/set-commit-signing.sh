88f49e15f76e7ca66ae11e834d3bd1d4e0237918
haeramkeem <ewqdsacxz2345@gmail.com>
2023-01-04 00:03:47 +0900

    example/git/config: set gpg commit signing as default

--- examples/git/config/set-commit-signing.sh
+++ examples/git/config/set-commit-signing.sh
@@ -3,3 +3,4 @@
 # Source: https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key#telling-git-about-your-ssh-key
 git config --global gpg.format ssh
 git config --global user.signingkey ~/.ssh/id_rsa.pub
+git config --global commit.gpgsign true
