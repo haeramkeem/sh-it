3016ed0d3d24896f2558b84b863b395145fdd457
haeramkim <haeram.kim1@si-analytics.ai>
2023-06-16 09:10:30 +0900

    [watcher] fix: http https healthcheck

--- watcher/http-https-healthcheck.sh
+++ watcher/http-https-healthcheck.sh
@@ -21,11 +21,11 @@ done
 
 function icmpcheck() {
     local ip=$1
-    local CONN_MSG="CONN"
+    local OK_MSG="CONN"
     local FAIL_MSG="FAIL"
 
     if `ping -c1 -W1 $ip &> /dev/null`; then
-        echo -n $CONN_MSG
+        echo -n $OK_MSG
     else
         echo -n $FAIL_MSG
     fi
@@ -34,25 +34,40 @@ function icmpcheck() {
 function tcpcheck() {
     local ip=$1
     local port=$2
-    local CONN_MSG="CONN"
+    local OK_MSG="CONN"
     local FAIL_MSG="FAIL"
 
     if `nc -z $ip $port &> /dev/null`; then
-        echo -n $CONN_MSG
+        echo -n $OK_MSG
     else
         echo -n $FAIL_MSG
     fi
 }
 
-MARGIN="\t\t"
-printf "IP${MARGIN}80/TCP${MARGIN}443/TCP${MARGIN}ENDPOINT\n"
-for e in $ENDPOINTS; do
-    icmpcheck $e
-    printf "$MARGIN"
+function certcheck() {
+    local ip=$1
+    local port=$2
+    local OK_MSG="VALID"
+    local FAIL_MSG="INVALID"
 
-    tcpcheck $e 80
-    printf "$MARGIN"
+    if `true | openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -checkend 0`; then
+        echo -n $OK_MSG
+    else
+        echo -n $FAIL_MSG
+    fi
+}
 
-    tcpcheck $e 443
-    printf "$MARGIN$e\n"
-done
+function http_code_check() {
+    local ip=$1
+    local scheme="https"
+    curl -s -o /dev/null -I -w "%{http_code}" $scheme://$ip
+}
+
+function print_table() {
+    echo "ENDPOINT|IP|80/TCP|443/TCP|CERTIFICATE|HTTPCODE"
+    for e in $ENDPOINTS; do
+        echo "${e}|$(icmpcheck $e)|$(tcpcheck $e 80)|$(tcpcheck $e 443)|$(certcheck $e 443)|$(http_code_check $e)"
+    done
+}
+
+print_table | column -t -s '|'
