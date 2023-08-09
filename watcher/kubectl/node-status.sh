5b669f56991878a2a1e9d32bb6d91e72236938f1
haeramkim <haeram.kim1@si-analytics.ai>
2023-08-09 09:36:57 +0900

    [watcher/kubectl] fix: node status

--- watcher/kubectl/node-status.sh
+++ watcher/kubectl/node-status.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 CTX=""
-OPTS=""
+OUT=""
+NODE=""
 
-while getopts 'c:wh' opt; do
+function helpme() {
+cat << EOF | column -t -s '|'
+ARG|VALUE|DESCRIPTION|REQUIRED|DEFAULT
+-c|kube-context|Kube context name|False|current context
+-N|node|Target node name|False|all nodes
+-W| |Wide output|False|
+-h| |Help|False|
+EOF
+}
+
+while getopts 'c:N:Wh' opt; do
     case "$opt" in
         c)
             CTX=$OPTARG
             ;;
-        w)
-            OPTS="$OPTS -owide"
+        N)
+            NODE=$OPTARG
+            ;;
+        W)
+            OUT="-o wide"
             ;;
         h)
-            echo "REQUIRED ARGS:"
-            echo "-c \${kubectl context}"
-            echo "-n \${node name}"
-            echo "OPTIONAL ARGS:"
-            echo "-w same with '-o wide'"
+            helpme
             exit 0
             ;;
         *)
@@ -25,6 +35,6 @@ while getopts 'c:wh' opt; do
     esac
 done
 
-kubectl --context $CTX get no $OPTS
+kubectl --context $CTX get no $NODE $OUT
 echo
-kubectl --context $CTX top no
+kubectl --context $CTX top no $NODE
