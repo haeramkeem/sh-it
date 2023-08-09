93d8687d69482a987e3d3aca8fd93ebae826da0d
haeramkim <haeram.kim1@si-analytics.ai>
2023-08-09 09:36:57 +0900

    [watcher/kubectl] fix: pod by node

--- watcher/kubectl/pod-by-node.sh
+++ watcher/kubectl/pod-by-node.sh
@@ -2,25 +2,48 @@
 
 NODE=""
 CTX=""
+NS="-A"
+OUTPUT=""
 
-while getopts 'c:n:h' opt; do
+function helpme() {
+cat << EOF | column -t -s '|'
+ARG|VALUE|DESCRIPTION|REQUIRED|DEFAULT
+-c|kube-context|Kube context name|False|current context
+-n|namespace|Namespace name|False|all namespace
+-N|node|Target node name|True|
+-W| |Wide output|False|
+-h| |Help|False|
+EOF
+}
+
+while getopts 'c:n:N:Wh' opt; do
     case "$opt" in
+        c)
+            CTX="--context $OPTARG"
+            ;;
         n)
+            NS="--namespace $OPTARG"
+            ;;
+        N)
             NODE=$OPTARG
             ;;
-        c)
-            CTX=$OPTARG
+        W)
+            OUTPUT="-o wide"
             ;;
         h)
-            echo "REQUIRED ARGS:"
-            echo "-c \${kubectl context}"
-            echo "-n \${node name}"
+            helpme
             exit 0
             ;;
         *)
-            echo "Unknown option '$opt'" 1>&2; exit 1
+            echo "Unknown option '$opt'" 1>&2
+            exit 1
             ;;
     esac
 done
 
-kubectl --context $CTX get po -A --field-selector spec.nodeName=$NODE
+if [[ $NODE == "" ]]; then
+    echo "Option '-N' is required" 1>&2
+    exit 1
+fi
+
+kubectl $CTX get po $NS $OUTPUT --field-selector spec.nodeName=$NODE
