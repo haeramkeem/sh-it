c8a1df69ef898a94c83bf9e9e53b71cc7210b8b4
haeramkeem <ewqdsacxz2345@gmail.com>
2023-08-10 09:49:56 +0900

    [] fix: fmt git commit

--- fmt-git-commit.sh
+++ fmt-git-commit.sh
@@ -12,7 +12,7 @@ fmt() {
     git add $f
     FNAME=$(rev <<< $f | cut -d '/' -f 1 | rev)
     FPATH=$(sed "s/$FNAME//g" <<< $f)
-    MSG=$(sed 's/.sh//g' <<< $FNAME | sed 's/-/ /g')
+    MSG=$(sed 's/\.sh//g' <<< $FNAME | sed 's/-/ /g')
     git commit -m "[${FPATH%/}] $p: $MSG"
 }
 
