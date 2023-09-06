60006365127cc95569746dc2f9a5147870d90f0f
haeramkim <haeram.kim1@si-analytics.ai>
2023-09-06 14:43:57 +0900

    [func] fix: tmux_flush

--- func/tmux_flush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function tmux_flush() {
-    tmux ls \
-        | grep -v attached \
-        | tr ':' ' ' \
-        | awk '{print $1}' \
-        | xargs -n1 tmux kill-session -t
-}
