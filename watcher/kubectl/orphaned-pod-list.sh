c29d95137778bfc4a287e442233c855f199756f4
haeramkim <haeram.kim1@si-analytics.ai>
2023-11-24 13:26:48 +0900

    [watcher/kubectl] fix: orphaned pod list

--- watcher/kubectl/orphaned-pod-list.sh
+++ watcher/kubectl/orphaned-pod-list.sh
@@ -2,23 +2,28 @@
 
 CTX=""
 NS=""
+OPT=""
 
 function helpme() {
 cat << EOF | column -t -s '|'
 ARG|VALUE|DESCRIPTION|REQUIRED|DEFAULT
 -c|kube-context|Kube context name|False|current context
 -n|namespace|Namespace name|False|current context namespace
+-W| |Wide output (same as '-o wide')|False|
 -h| |Help|False|
 EOF
 }
 
-while getopts 'c:n:h' opt; do
+while getopts 'c:n:Wh' opt; do
     case "$opt" in
         c)
             CTX="--context $OPTARG"
             ;;
         n)
             NS="--namespace $OPTARG"
+            ;;
+        W)
+            OPT="$OPT -o wide"
             ;;
         h)
             helpme
@@ -34,7 +39,7 @@ done
 KUBE_CMD="kubectl $CTX $NS get pod -o yaml"
 YQ_QUERY=".items.[] | select(.metadata.ownerReferences == null) | .metadata.name"
 if [[ "$(bash -c "$KUBE_CMD | yq '$YQ_QUERY' | wc -l | xargs")" != "0" ]]; then
-    bash -c "$KUBE_CMD | yq '$YQ_QUERY'"
+    kubectl $CTX $NS get pod $(bash -c "$KUBE_CMD | yq '$YQ_QUERY'") $OPT
 else
     echo "Orphaned pod not found."
 fi
