ecb3c98380d20180f712829539f36659d5fa0685
haeramkeem <ewqdsacxz2345@gmail.com>
2024-03-05 00:01:11 +0900

    [examples/tr] fix: basic usage

--- examples/tr/basic-usage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source: https://stackoverflow.com/a/1252010
-
-# Replace file content
-cat $FILE | tr ${OLD_CHAR} ${NEW_CHAR}
-
-# Replace STDIN
-tr ${OLD_CHAR} ${NEW_CHAR} <<< "${STRING}"
