7e860414dad07347b49015723266c01839b778e4
haeramkeem <ewqdsacxz2345@gmail.com>
2024-09-15 22:17:35 +0900

    [examples/journalctl] fix: filter logs

--- examples/journalctl/filter-logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Source: https://man7.org/linux/man-pages/man1/journalctl.1.html
-
-# journalctl hacks
-#   `-p`, `--priority`: Set log severities
-#   `--facility`: Set log facilities
-journalctl -p 0 --facility 0
