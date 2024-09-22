b8efdc1e99f89e4db81398a9b0b1e6464f2b2c6d
haeramkeem <ewqdsacxz2345@gmail.com>
2024-09-23 08:42:02 +0900

    [examples/psql-cli] fix: list all users

--- examples/psql-cli/list-all-users.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Command `\du` prints all databases in the postgres
-psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -c '\du'
-
-# `\du+` for more information
-psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -c '\du+'
