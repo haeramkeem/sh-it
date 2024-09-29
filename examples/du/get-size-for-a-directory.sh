fdba6085ad5138ba39ec8540bcf747632921bbc1
haeramkeem <ewqdsacxz2345@gmail.com>
2024-09-30 08:14:45 +0900

    [examples/du] fix: get size for a directory

--- examples/du/get-size-for-a-directory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Source: https://linuxize.com/post/how-get-size-of-file-directory-linux/
-# -h: human-readable (print size with GiB, MiB, etc)
-# -s: show only root dir size
-du -sh ${DIR}
