4dd07d26e1ee2b221e50431a04260b5f221154aa
haeramkeem <ewqdsacxz2345@gmail.com>
2024-09-30 08:29:59 +0900

    [examples/wall] fix: basic usage

--- examples/wall/basic-usage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The wall message is to broadcast the message to the all users of the linux
-# This is commonly use when some events like errors happen
-# U can use this wall message with the `wall` command
-wall "damn!"
